e9eba2217e0d3ad523f15d03945feaac66d025de
adayperdomo <148550944+adayperdomo@users.noreply.github.com>
2025-01-21 13:35:40 +0000

    Add files via upload

---
+++ Ejercicios-Java-UT5/EjerciciosUT5.java
@@ -0,0 +1,18 @@
+package com.mycompany.mavenproject3;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author Alumno
+ */
+public class EjerciciosUT5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
