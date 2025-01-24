03f11dac1f22d701877fafe5c2fbd60efd32d8b4
adayperdomo <148550944+adayperdomo@users.noreply.github.com>
2025-01-24 08:21:03 +0000

    Update CuadradoHueco.java

--- ut5/CuadradoHueco.java
+++ ut5/CuadradoHueco.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.ejercicios.java.ut5;
+package com.mycompany.mavenproject3;
 
 /**
  *
- * @author Usuario
+ * @author Alumno
  */
 public class CuadradoHueco {
     private int numero;
@@ -15,25 +15,29 @@ public class CuadradoHueco {
     }
     
     public void dibujarCuadrado(){
-        for (int i = 1; i <= numero; i++) {
-            
-            for (int j = 1; j <= numero; j++) {
-                if (i == 1 || i == numero) {
-                    System.out.print("-");
-                }else{
-                    if (j == 1 || j == numero) {
-                        System.out.print("-");
-                    }else{
-                        System.out.print(" ");
-                    }
-                }
-            }
-            System.out.println();
+        //Primera linea
+        for (int i = 0; i < numero+2; i++) {
+        System.out.print("*");
         }
+        System.out.println(); //Salto de linea
+        
+        //lineas intermedias
+        for (int i = 0; i <= numero-2; i++) {
+        System.out.print("*");//primer asterisco
+            for (int j = 0; j < numero; j++) {
+            System.out.print(" ");//espacio en medio
+        }
+        System.out.print("*");//ultimo asterisco
+        System.out.println();
+        }
+        for (int i = 0; i < numero+2; i++) {
+        System.out.print("*");//ultimo asterisco y salto de linea
+        }
+        System.out.println();//salto de linea
     }
     
-    public static void main(String[] args) {
-        CuadradoHueco cuad = new CuadradoHueco(6);
-        cuad.dibujarCuadrado();
+    public static void main(String[] args){
+        CuadradoHueco cua = new CuadradoHueco(4);
+        cua.dibujarCuadrado();
     }
 }
